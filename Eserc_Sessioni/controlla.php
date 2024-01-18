95d832a912297cd0287b93a3a3265a6d51bc8012
Marco Armenise <armenise.marco@panettipitagora.edu.it>
2024-01-18 16:47:38 +0100

    Modificati file
    - Aggiunte pagine 1-2-3
    - Modificata pagina Menu
    - Modificata pagina di controllo

--- Eserc_Sessioni/controlla.php
+++ Eserc_Sessioni/controlla.php
@@ -3,12 +3,18 @@
     $usernameCorretto = "utente";
     $passwordCorretta = "1234";
 
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if (isset($_POST["login"])) {
 
         $username = $_POST["username"];
         $password = $_POST["password"];
 
         if ($username == $usernameCorretto && $password == $passwordCorretta) {
+
+            session_start();
+
+            $_SESSION["count_p1"] = 0;
+            $_SESSION["count_p2"] = 0;  
+            $_SESSION["count_p3"] = 0;
 
             echo "<h3>Bravo ti sei loggato!</h3>";
             echo '<a href="menu.php"><button>Continua</button></a>';
---
+++ Eserc_Sessioni/pagina3.php
@@ -0,0 +1,13 @@
+<?php
+
+    session_start();
+
+    $id = session_id(); 
+
+    $_SESSION["count_p3"] = $_SESSION["count_p3"] + 1;
+
+    echo "BENVENUTO nella PAGINA - 1 - del SITO <br><br>";
+    echo "Ciao, questa pagina e' stata visita da te <b>" . $_SESSION["count_p1"] . "</b> volte. <br><br>";
+    echo "Il tuo codice identificativo in questa sessione e': <b>".$id."</b> <br><br>";
+
+?>
