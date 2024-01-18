13debdad4ff8a09da25399236588ac9aa013d340
Marco Armenise <armenise.marco@panettipitagora.edu.it>
2024-01-18 16:32:54 +0100

    Modificato file
    Modificato e aggiunto menu pagine e uscita dal sito

---
+++ Eserc_Sessioni/menu.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Menu</title>
+</head>
+<body>
+    
+    <h2>BENVENUTO nel nostro SPLENDIDO SITO</h2>
+
+    <ul>
+        <li><a href="pagina1.php">Pagina 1</a></li>
+        <li><a href="pagina2.php">Pagina 2</a></li>
+        <li><a href="pagina3.php">Pagina 3</a></li> <br>
+        <li><a href="logout.php">ESCI DAL SITO</a></li>
+    </ul>
+
+</body>
+</html>
